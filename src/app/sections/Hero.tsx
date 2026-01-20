c43689bab81a965843d8f00ca91ca5d0747dc550
Karthik R <karthikeya.rachamolla@gmail.com>
2026-01-20 12:01:12 +0530

    changing bg back to original

--- src/app/sections/Hero.tsx
+++ src/app/sections/Hero.tsx
@@ -149,20 +149,7 @@ export default function HeroSection() {
             className="object-contain"
           />
         </div>
-        <div
-          className="absolute top-[70%] right-[60%] w-[40vh] h-64 opacity-70"
-          data-speed="0.8"
-        > 
-          <Image
-            src="/cloud_final1.webp"
-            alt=""
-            fill
-            className="object-contain"
-            sizes="(max-width: 768px) 100vw, 500px"
-            loading="lazy"
-          />
-        </div>
-      </div> */}
+      </div>
 
       {/* Content */}
       <div className="w-full max-w-6xl mx-auto relative pointer-events-none pb-8 sm:pb-14 lg:pb-16 z-40">
