4ee68cc88f0afdc73f0dd009945ea2dd58a015fc
Mohamed Hedi <81655926+MohamedHedi1998@users.noreply.github.com>
2026-03-11 00:01:31 +0100

    Removed #faq

--- src/app/components/NavigationBar.tsx
+++ src/app/components/NavigationBar.tsx
@@ -303,7 +303,7 @@ export function NavigationBar() {
                 <NavigationMenuItem>
                   <NavigationMenuLink asChild>
                     <a
-                      href="#faq"
+                      href="#"
                       onClick={handleRegisterClick}
                       className="cursor-pointer font-mono text-sm sm:text-base md:text-lg text-white bg-transparent hover:bg-transparent focus:bg-transparent relative after:absolute after:bottom-0 after:left-0 after:w-0 after:h-0.5 after:bg-[#19E363] after:transition-all after:duration-300 hover:after:w-full"
                     >
