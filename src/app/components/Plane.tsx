432c078ecbe11ec96aea0b51e7e46367015c3cf1
Amaan Bilwar <bilwarad@mail.uc.edu>
2026-03-20 04:37:31 +0530

    fix plane error

--- src/app/components/Plane.tsx
+++ src/app/components/Plane.tsx
@@ -2,7 +2,7 @@
 
 import { useEffect, useRef } from "react";
 import * as THREE from "three";
-import { GLTFLoader } from "three/examples/jsm/loaders/GLTFLoader";
+import { GLTFLoader } from "three/examples/jsm/loaders/GLTFLoader.js";
 import { gsap } from "gsap";
 
 const Plane = () => {
